29c6fac042c2478a9567f043edb80c38c8e05087
Junior <changjiale@gmail.com>
2021-10-06 19:36:06 +0800

    added leafet map

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-let singapore = [1.29,103.85];
-let map = L.map('map').setView(singapore,13);
+
+
+// let singapore = [1.29, 103.85];
+ // Singapore latlng
+// let map = L.map('map').setView(singapore, 13);
+// map.setMaxBounds(map.getBounds());
+// map.setMinZoom(12);
+
+const mymap = L.map('map').setView([ 1.29,103.85], 13);
+
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -8,4 +16,6 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
-}).addTo(map);
+}).addTo(mymap);
+
+
