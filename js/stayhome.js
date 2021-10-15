fb9b9d76c44257c35a315c1e4fcffa1caca50fec
Junior <changjiale@gmail.com>
2021-10-15 19:32:31 +0800

    added photos for stayhome.html portion

--- js/stayhome.js
+++ js/stayhome.js
@@ -1,5 +1,23 @@
 function homestay(){
-    console.log('counting');
+    const inputElems = document.getElementsByTagName("input");
+
+
+    count = 0
+    for ( var i =0; i < inputElems.length; i++) {
+        var x = inputElems[i]
+        if (x.value == 'yes' && (x.checked ) ) {
+            count++;
+            // console.log(x.value)
+        }
+    }
+    console.log(count)
+    if (count <=  4) {
+        document.getElementsById("stayhome").src="../assets/redcross.jpg";
+        console.log('Not Eligible');
+    } else {
+        document.getElementById("stayhome").src="../assets/greentick.jpg"
+        console.log("You are Eligible");
+    }
     
     
 }
--- readme.md
+++ readme.md
@@ -1,2 +1,7 @@
+<h1>Resources </h1>
+
+greentick
+https://www.vectorstock.com/royalty-free-vector/green-tick-check-mark-icon-simple-style-vector-8375981
+
 rLogo
 https://logopond.com/younique/showcase/detail/293627
